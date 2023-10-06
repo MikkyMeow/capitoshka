a94357580be7bb0462a1ae8f39740656e2469bfd
Artur Akhmadullin <arturakk2015@gmail.com>
2023-10-06 15:04:51 +0300

    added player, unstudied tabs

--- src/components/pages/EpisodeList/ui/EpisodeList.tsx
+++ src/components/pages/EpisodeList/ui/EpisodeList.tsx
@@ -1,4 +1,6 @@
 import {
+  Button,
+  ButtonGroup,
   Checkbox,
   IconButton,
   List,
@@ -14,6 +16,7 @@ import { series } from "../../../../content";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { AppBar } from "components/widgets/AppBar";
+import { Player } from "components/widgets/Player";
 
 export const EpisodeList = () => {
   const { seriesId, seasonId, episodeId } = useParams();
@@ -23,10 +26,10 @@ export const EpisodeList = () => {
   );
   // @ts-ignore
   const currentEpisode = series[seriesId].seasons[seasonId][episodeId];
+  const [type, setType] = useState<"all" | "unstudied" | "player">("all");
 
   const checkboxHandler = (index: number) => {
     let foundIndex = selectedSubtitles.indexOf(index);
-    console.log(foundIndex);
     if (foundIndex !== -1) {
       setSelectedSubtitles([
         ...selectedSubtitles.slice(0, foundIndex),
@@ -55,45 +58,75 @@ export const EpisodeList = () => {
     );
   }, [selectedSubtitles, seriesId, seasonId, episodeId]);
 
+  useEffect(() => {}, [type]);
+
   return (
     <>
       <AppBar />
       <Paper sx={{ padding: "12px", marginBottom: "8px" }}>
         <h2>{currentEpisode.name}</h2>
+        <ButtonGroup>
+          <Button
+            variant={type === "all" ? "contained" : "outlined"}
+            onClick={() => setType("all")}
+          >
+            All
+          </Button>
+          <Button
+            variant={type === "unstudied" ? "contained" : "outlined"}
+            onClick={() => setType("unstudied")}
+          >
+            Unstudied
+          </Button>
+          <Button
+            variant={type === "player" ? "contained" : "outlined"}
+            onClick={() => setType("player")}
+          >
+            Player
+          </Button>
+        </ButtonGroup>
       </Paper>
       <Paper>
         <List>
-          {currentEpisode.subtitles.map((value: any, index: number) => {
-            const labelId = `checkbox-list-label-${value}`;
-            const isSelected = selectedSubtitles.includes(index);
+          {type !== "player" ? (
+            currentEpisode.subtitles.map((value: any, index: number) => {
+              const labelId = `checkbox-list-label-${value}`;
+              const isSelected = selectedSubtitles.includes(index);
+              if (type === "unstudied" && isSelected) return null;
 
-            return (
-              <ListItem
-                key={Math.random()}
-                disablePadding
-                onClick={() => checkboxHandler(index)}
-                secondaryAction={
-                  <Tooltip title={value.rus} placement="top-start">
-                    <IconButton edge="end" aria-label="comments">
-                      <CommentOutlined />
-                    </IconButton>
-                  </Tooltip>
-                }
-              >
-                <ListItemButton disableRipple dense>
-                  <ListItemIcon>
-                    <Checkbox
-                      edge="start"
-                      checked={isSelected}
-                      tabIndex={-1}
-                      disableRipple
-                    />
-                  </ListItemIcon>
-                  <ListItemText id={labelId} primary={value.eng} />
-                </ListItemButton>
-              </ListItem>
-            );
-          })}
+              return (
+                <ListItem
+                  key={Math.random()}
+                  disablePadding
+                  onClick={() => checkboxHandler(index)}
+                  secondaryAction={
+                    <Tooltip title={value.rus} placement="top-start">
+                      <IconButton edge="end" aria-label="comments">
+                        <CommentOutlined />
+                      </IconButton>
+                    </Tooltip>
+                  }
+                >
+                  <ListItemButton disableRipple dense>
+                    <ListItemIcon>
+                      <Checkbox
+                        edge="start"
+                        checked={isSelected}
+                        tabIndex={-1}
+                        disableRipple
+                      />
+                    </ListItemIcon>
+                    <ListItemText id={labelId} primary={value.eng} />
+                  </ListItemButton>
+                </ListItem>
+              );
+            })
+          ) : (
+            <Player
+              episode={currentEpisode.subtitles}
+              selected={selectedSubtitles}
+            />
+          )}
         </List>
       </Paper>
     </>
--- src/components/widgets/Player/ui/Player.tsx
+++ src/components/widgets/Player/ui/Player.tsx
@@ -1,39 +1,72 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
-import { episode1 } from "../../../../content/treePlusOne/season1/episode1";
+import { FC, useCallback, useEffect, useMemo, useRef, useState } from "react";
 
-export const Player = () => {
+interface IProps {
+  episode: { eng: string; rus: string }[];
+  selected: number[];
+}
+// const synth = window.speechSynthesis;
+// const [pos, setPos] = useState(0);
+
+// const eng = useMemo(
+//   () => new SpeechSynthesisUtterance(episode[pos].eng),
+//   [pos, episode]
+// );
+// const rus = useMemo(
+//   () => new SpeechSynthesisUtterance(episode[pos].rus),
+//   [pos, episode]
+// );
+// eng.lang = "en";
+// rus.lang = "ru";
+// eng.rate = 0.7;
+
+// const play = useCallback(() => {
+//   synth.speak(eng);
+//   synth.speak(rus);
+// }, [eng, synth, rus]);
+
+// rus.addEventListener("end", () => {
+//   if (pos < episode.length - 1) {
+//     setPos(pos + 1);
+//   }
+// });
+
+// useEffect(() => {
+//   if (pos < episode.length) {
+//     // play();
+//   }
+// }, [pos, play, episode.length]);
+
+// useEffect(() => {
+//   console.log("mounted");
+//   return () => console.log("unmounted");
+// }, []);
+
+export const Player: FC<IProps> = ({ episode }) => {
   const synth = window.speechSynthesis;
   const [pos, setPos] = useState(0);
 
-  const eng = useMemo(() => new SpeechSynthesisUtterance(episode1[pos].eng), [pos]);
-  const rus = useMemo(() => new SpeechSynthesisUtterance(episode1[pos].rus), [pos]);
-  eng.lang = 'en';
-  rus.lang = 'ru';
-  eng.rate = .7;
-  
-  const play = useCallback(() => {
+  const eng = new SpeechSynthesisUtterance(episode[pos].eng);
+  const rus = new SpeechSynthesisUtterance(episode[pos].rus);
+
+  eng.lang = "en";
+  rus.lang = "ru";
+
+  const play = () => {
     synth.speak(eng);
     synth.speak(rus);
-  }, [eng, synth, rus]);
+  };
 
-  rus.addEventListener('end', () => {
-    if (pos < episode1.length - 1) {
-      setPos(pos + 1);
-    }
-  })
+  const pause = () => {
+    synth.cancel();
+  };
 
-  useEffect(() => {
-    if (pos < episode1.length) {
-      play();
-    }
-  }, [pos, play]);
-  
   return (
     <div>
       <h1>Position: {pos}</h1>
-      <p>{episode1[pos].eng}</p>
-      <p>{episode1[pos].rus}</p>
+      <p>{episode[pos].eng}</p>
+      <p>{episode[pos].rus}</p>
       <button onClick={() => play()}>play</button>
+      <button onClick={() => pause()}>pause</button>
     </div>
-  )
-}
+  );
+};
